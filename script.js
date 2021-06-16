e505e42d216764b9cbd3a7cbbc897ab763f87d20
mkrobs <dev@mikaelr.com>
2021-06-16 21:09:20 +0300

    16 juni

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-Javascript
+function checkDriverAge(age){
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too young to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering on. Enjoy the ride!");
+	} else if (Number(age) === 18){
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+
+var checkDriverAge2 = function(){
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too young to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering on. Enjoy the ride!");
+	} else if (Number(age) === 18){
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+function checkDriverAge3(age){
+	if (Number(age) < 18) {
+		return "Sorry, you are too young to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering on. Enjoy the ride!";
+	} else if (Number(age) === 18){
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
